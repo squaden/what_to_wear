7db8f47e9f174bcf96b8b8e5317bf8c100eec073
squaden <druzh.ds@gmail.com>
2019-06-25 20:51:05 +0300

    First corrections

--- lib/wardrobe.rb
+++ lib/wardrobe.rb
@@ -10,9 +10,9 @@ class Wardrobe
     @garments = garments
   end
 
-  def for_temperatures!(temperatures)
+  def for_temperatures!(user_input)
     @garments.select! do |garment|
-      garment.suits_for?(temperatures)
+      garment.suits_for?(user_input)
     end
   end
 
